e3724faf4563e94a67e3444b71a5a57ff4302e38
tsthuy <huydo282003@gmail.com>
2024-12-20 23:59:38 +0700

    feat: add axios tool

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Button } from '~/components/ui';
+
+
+export default function Products () {
+  return (
+    <div>
+        <Button className='text-red-500'>Test shadcn</Button>
+      <h1>hello am new from nextjs</h1>
+    </div>
+  );
+}
---
+++ src/service/axios-instance.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+// Create axios instance
+const axiosInstance = axios.create({
+  baseURL: process.env.API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 10000,
+});
+
+// Interceptor cho request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Interceptor cho response
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error('Unauthorized access - 401');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
